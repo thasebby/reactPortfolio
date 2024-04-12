3cff9a37627aa0a6f679f1670b00d284ce2da693
Sebastian Saenz <sebastian.saenz1203@gmail.com>
2024-04-12 13:11:37 -0400

    everything needed for the project was included

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# reactPortfolio
+# React Portfolio
+
+## Description
+I created a portfolio to display some projects I have done, a short description of who I am, a way to download my resume and a way to communicate with me. All of this was done using React.
+
+## Steps for build
+* Used bootstrap for styling
+
+* Created the whole project using a React framework
+
+* I am able to display projects with direct links to GitHub repo and live website links
+
+
+## Installation 
+Run an 'npm i' to install all necessary dependencies and 'npm start' will start the code. This will send the user to a deployed website on a local host.
+
+
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,7 +1,127 @@
 import resume from '../assets/sebastianResume.pdf'
+import React, { useState } from 'react'
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+
+function FrontEndModal(props) {
+    const { onHide } = props;
+    return (
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header className='bg-dark' closeButton>
+                <Modal.Title className="text-light" id='contained-modal-title-vcenter'>
+                    Front-End Proficiencies
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body className='dark'>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>Understanding of different CSS frameworks</li>
+                    <li>jQuery</li>
+                    <li>RESTful Services/APIs</li>
+                    <li>Website Responsiveness</li>
+                    <li>GIT/Version Control</li>
+                </ul>
+            </Modal.Body>
+            <Modal.Footer className='dark'>
+                <Button onClick={onHide}>Close</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
+
+function BackEndModal(props) {
+    const { onHide } = props;
+    return (
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header className='bg-dark' closeButton>
+                <Modal.Title className="text-light" id='contained-modal-title-vcenter'>
+                    Back-End Proficiencies
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body className='dark'>
+                <ul>
+                    <li>Node.js</li>
+                    <li>Express</li>
+                    <li>MySQL</li>
+                    <li>MongoDB</li>
+                    <li>Understanding and manipulating JWT</li>
+                    <li>Mongoose</li>
+                    <li>Apollo Client</li>
+                    <li>Sequelize</li>
+                </ul>
+            </Modal.Body>
+            <Modal.Footer className='dark'>
+                <Button onClick={onHide}>Close</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
 
 export default function Resume() {
-    return(
 
+    const [modalShow, setModalShow] = useState(false);
+
+    const onButtonClick = () => {
+        const pdfUrl = resume;
+        const link = document.createElement('a');
+        link.href = pdfUrl;
+        link.download = 'SebastianSaenzResume.pdf';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+
+    return (
+        <div className='row justify-content-center p-4'>
+            <h1 className='text-center col-3 pb-3 mb-4 border-bottom'>Sebastian Saenz's Resume</h1>
+            <button onClick={onButtonClick}>
+                Download my Resume!
+            </button>
+
+            <section className='d-flex flex-wrap justify-content-center text-center'>
+                <h1>Developer's Profeciencies</h1>
+
+                <>
+                    <div className='card'>
+                        <Button variant="primary" onClick={() => setModalShow(true)}>
+                            Front-End Web Development
+                        </Button>
+
+                        <FrontEndModal
+                            show = {modalShow}
+                            onHide = {() => setModalShow(false)}
+                        />
+                    </div>
+                </> 
+
+                <>
+                    <div className='card'>
+                        <Button variant="primary" onClick={() => setModalShow(true)}>
+                            Back-End Web Development
+                        </Button>
+
+                        <BackEndModal
+                            show = {modalShow}
+                            onHide = {() => setModalShow(false)}
+                        />
+                    </div>
+                </>
+
+            </section>
+
+        </div>
     );
 }
