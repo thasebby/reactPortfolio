58c22d1733e2f1dd83d91bb2e5c4859c890a169c
Sebastian Saenz <sebastian.saenz1203@gmail.com>
2024-06-12 03:23:29 -0400

    spread out the home button and the rest of the tabs

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -7,45 +7,52 @@ function NavTabs() {
   const currentPage = useLocation().pathname;
 
   return (
-    <ul className="nav nav-tabs justify-content-end jaro-title" style={{ borderBottom: '0px'}}>
-      <li className="nav-item">
-        <Link
-          to="/"
-          // This is a conditional (ternary) operator that checks to see if the current page is "Home"
-          // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-          className={`nav-color ${currentPage === '/' ? 'nav-link active' : 'nav-link'}`}
-        >
-          Home
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/Portfolio"
-          // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={`nav-color ${currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}`}
-        >
-          Portfolio
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/Resume"
-          // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={`nav-color ${currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}`}
-        >
-          Resume
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/Contact"
-          // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={`nav-color ${currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}`}
-        >
-          Contact
-        </Link>
-      </li>
-    </ul>
+    <nav className='nav-container'>
+      <ul className="nav nav-tabs home-link jaro-title" style={{ borderBottom: '0px' }}>
+        <li className="nav-item">
+          <Link
+            to="/"
+            // This is a conditional (ternary) operator that checks to see if the current page is "Home"
+            // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
+            className={`nav-color ${currentPage === '/' ? 'nav-link active' : 'nav-link'}`}
+          >
+            Sebastian Saenz
+          </Link>
+        </li>
+      </ul>
+
+      <ul className='nav nav-tabs other-links jaro-title' style={{ borderBottom: '0px' }}>
+        <li className="nav-item">
+          <Link
+            to="/Portfolio"
+            // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+            className={`nav-color ${currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}`}
+          >
+            Portfolio
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link
+            to="/Resume"
+            // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+            className={`nav-color ${currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}`}
+          >
+            Resume
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link
+            to="/Contact"
+            // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+            className={`nav-color ${currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}`}
+          >
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
